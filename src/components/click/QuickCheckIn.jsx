f59c0febd63f98c4c9edb886d1dd85dcf5365879
Your Name <babulhossan.info@gmail.com>
2026-04-17 08:13:35 +0600

    context

---
+++ src/components/click/QuickCheckIn.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const QuickCheckIn = () => {
+    return (
+        <div>
+            hello
+        </div>
+    );
+};
+
+export default QuickCheckIn;
---
+++ src/context/context.jsx
@@ -0,0 +1,24 @@
+
+"use client";
+import { createContext, useEffect, useState } from "react";
+
+export const AppContext = createContext();
+
+export const InstallAppContext = ({children}) => {
+    const [item, setItem] = useState([]);
+
+    useEffect(() => {
+        fetch("/data.json")
+        .then((res) => res.json())
+        .then(data => setItem(data))
+        .catch((err) => console.error("Error loading data:", err));
+}, []);
+    
+    return (
+        <AppContext.Provider value={{item, setItem}}>
+{children}
+        </AppContext.Provider>
+    );
+};
+
+export default InstallAppContext;
